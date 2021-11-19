816c9e9cc6bb69a26030a44134eea29b6db88017
R <73017078+Inletbr@users.noreply.github.com>
2021-11-19 15:52:12 +0300

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+#Binary search
+
+name = input("What is your name?\n")
+print("Nice to meet you,", name,"!")
+print("The program guesses a number from 0 to N")
+n = int(input("Enter the N:  \n"))
+what = 0
+low = 0
+high = n
+while low <= high:
+    mid = (low + high) // 2
+    print(mid)
+    what = input("more, less, equal? \n")
+    if what == "more":
+        low = mid
+        mid = (low + high) // 2
+    elif what == "less":
+        high = mid
+        mid = high // 2
+    elif what == "equal":
+        print("Succes", mid)
+        break
